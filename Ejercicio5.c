b04e8adeb1240f0bf32d707c991b97c4109b020f
Uli__ <ulisescolin332@gmail.com>
2025-10-17 15:22:27 -0600

    Update Ejercicio5.c

--- Ejercicio5.c
+++ Ejercicio5.c
@@ -3,30 +3,33 @@ Clase 2, ejercicio 5*/
 
 #include <stdio.h>
 #include <math.h>
+
 void main()
 {
     int edad; 
     const int base = 7;
     float precio;
+    
     printf ("Ingrese su edad: \n");
     scanf ("%d", &edad);
+    
     if (edad>=0)
     {
         if (edad>=60)
         {
-            precio = 7-(base * 0.55);
-            printf ("Costo de la entrada = %f euros", precio);
+            precio = base * 0.45;
+            printf ("Costo de la entrada = %f euros \n", precio);
         }
         else
         {
             if (edad<60 && edad>5)
             {
-               printf ("Costo de la entrada = %d euros", base); 
+               printf ("Costo de la entrada = %d euros \n", base); 
             }
             else
             {
-                precio = 7-(base * 0.60);
-                printf ("Costo de la entrada = %f euros", precio);
+                precio = base * 0.40;
+                printf ("Costo de la entrada = %f euros \n", precio);
             }
         }
     }
