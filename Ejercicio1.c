2055dc532b3fc4ab2cdd82131c7a2b2b4cb1ed54
Uli__ <ulisescolin332@gmail.com>
2025-10-17 13:27:31 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,14 @@
+/* Francisco Coliín Ulises
+Clase 2, ejercicio 1*/
+
+#include <stdio.h>
+void main()
+{
+  int num;
+  printf ("Dame un numero: \n");
+  scanf ("%d", &num);
+  if (num > 100)
+  {
+    printf ("El numero es mayor a 100");
+  }
+}
