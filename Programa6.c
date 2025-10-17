caafaa6278d8309a101922196dbd06f2ecb4738b
Uli__ <ulisescolin332@gmail.com>
2025-10-17 15:51:50 -0600

    Create Programa6.c

---
+++ Programa6.c
@@ -0,0 +1,23 @@
+/* Francisco Coliín Ulises
+Clase 2, ejercicio 6*/
+
+#include <stdio.h>
+
+void main()
+{
+    float val, fval;
+    
+    printf ("Ingrese un valor x de la funcion: \n");
+    scanf ("%f", &val);
+    
+    if (val<=0)
+    {
+        fval = val + 3;
+        printf ("La funcion f(x) valuda en el valor de x, f(%f) = %f \n", val, fval);
+    }
+    else
+    {
+        fval = (val * val) + (2 * val);
+        printf ("La funcion f(x) valuda en el valor de x, f(%f) = %f \n", val, fval);
+    }
+}
