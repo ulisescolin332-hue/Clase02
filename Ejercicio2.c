6311a721e19075f1d06878965a3b3d9b7812a27a
Uli__ <ulisescolin332@gmail.com>
2025-10-17 13:43:11 -0600

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -1,13 +1,16 @@
-
+/* Francisco Coliín Ulises
+Clase 2, ejercicio 2*/
 
 #include <stdio.h>
 void main()
 {
-  int num;
-  printf ("Dame un numero: \n");
-  scanf ("%d", &num);
-  if (num > 100)
+  int num1, num2;
+  printf ("Ingrese el primer numero: \n");
+  scanf ("%d", &num1);
+  printf ("Ingrese el segundo numero: \n");
+  scanf ("%d", &num2);
+  if (num1 % num2 == 0)
   {
-    printf ("El numero es mayor a 100");
+    printf ("El numero es divisible");
   }
 }
