7c09e08c77f7625ff81cbf05cc0d3b633528cfb2
Uli__ <ulisescolin332@gmail.com>
2025-10-19 10:24:41 -0600

    Update Ejercicio10.c

--- Ejercicio10.c
+++ Ejercicio10.c
@@ -1,63 +1,72 @@
-/* Francisco Colín Ulises 
-Clase 2, ejercicio 10*/
-
 #include <stdio.h>
-
-void main()
+int main()
 {
-	int num1, num2, num3, m, mm, mmm; 
-
-	printf ("Ingrese un numero: \n");
-	scanf ("%d", &num1);
-	printf ("Ingrese otro numero: \n");
-	scanf ("%d", &num2);
-	printf ("Ingrese otro numero: \n");
-	scanf ("%d", &num3);
-
-	if (num2<=num3)
-	{
-	    mmm = num3;
-	    if (num1<=num2)
-	    {
-	        mm = num2;
-	        m = num1;
-	    }
-	    else
-	    {
-	        mm = num1;
-	        m = num2;
-	    }
-	}
-	else
-	{
-		if (num3<=num2)
-	    {
-		    mmm = num2;
-		    if (num1<=num3)
-	        {
-	            mm = num3;
-	            m = num1;
-	        }
-	        else
-	        {
-	            mm = num1;
-	            m = num3;
-	        }
-	    }
-	    else
-	    {
-	        mmm = num1;
-	        if (num2<=num3)
-	        {
-	            mm = num3;
-	            m = num2;
-	        }
-	        else
-	        {
-	            mm = num2;
-	            m = num3;
-	        }
-	    }
-	}
+    int a, b, c, m, mm, mmm;
+    
+    printf("Ingrese un valor: ");
+    scanf("%d",&a);
+    printf("Ingrese otro valor: ");
+    scanf("%d",&b);
+    printf("Ingrese otro valor: ");
+    scanf("%d",&c);
+    
+    if (b<=a && c<=a)
+    {
+        mmm=a;
+        if (c<=b)
+        {
+            mm=b;
+            m=c;
+        }
+        else
+        {
+            if (b<=c)
+            {
+                mm=c;
+                m=b;
+            }
+        }
+    }
+    else
+    {
+        if (c<=b && a<=b)
+        {
+            mmm=b;
+            if (c<=a)
+            {
+                mm=a;
+                m=c;
+            }
+            else
+            {
+                if (a<=c)
+                {
+                    mm=c;
+                    m=a;
+                }
+            }
+        }
+        else
+        {
+            if (b<=c && a<=c)
+            {
+                mmm=c;
+                if (a<=b)
+                {
+                    mm=b;
+                    m=a;
+                }
+                else
+                {
+                    if (b<=a)
+                    {
+                        mm=a;
+                        m=b;
+                    }
+                }
+            }
+        }
+    }
     printf ("Los numeros ordenados de mayor a menor: %d => %d => %d", mmm, mm, m);
+    return 0;
 }
