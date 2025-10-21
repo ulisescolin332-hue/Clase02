4ca0df33d500b7f00dd506a92a357d1202bc8009
Uli__ <ulisescolin332@gmail.com>
2025-10-20 22:48:17 -0600

    Update Ejercicio12.c

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -4,32 +4,32 @@ Clase 2, ejercicio 12 */
 #include <stdio.h>
 int main()
 {
-    char num;
+    int op;
     
     printf("Ingrese algun numero del 1 al 7 para asignar un dia: \n");
-    scanf(" %c", &num);
+    scanf("%d", &op);
 
-	switch (num)
+	switch (op)
 	{
-		case '1':
+		case 1:
 			printf("\nLunes\n");
 			break;
-        case '2':
+        case 2:
         	printf("\nMartes\n");
 			break;
-        case '3':
+        case 3:
         	printf("\nMiercoles\n");
 			break;
-        case '4':
+        case 4:
         	printf("\nJueves\n");
 			break;
-        case '5':
+        case 5:
         	printf("\nViernes\n");
 			break;
-        case '6':	
+        case 6:	
 			printf("\nSabado\n");
 			break;
-		case '7':
+		case 7:
 			printf("\nDomingo\n");
 			break;
 		
